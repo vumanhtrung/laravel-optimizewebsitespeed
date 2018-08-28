928ea60f7ce144ad3b61074de15d4b3b0b972f7f
Vu Manh Trung <manhtrungit8x@gmail.com>
2018-08-28 12:14:45 +0700

    first commit

---
+++ resources/views/data.blade.php
@@ -0,0 +1,50 @@
+<!-- data.blade.php -->
+<!DOCTYPE html>
+<html>
+<head>
+    <title>How to Optimize Website Speed and Performance in Laravel</title>
+    <link rel="stylesheet" href="{{asset('css/app.css')}}">
+    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" rel="stylesheet">
+</head>
+<body>
+<div class="container">
+    <h1>Optimize Website Speed and Performance in Laravel</h1>
+    <table class="table table-bordered">
+        <thead>
+          <tr>
+            <th>Id</th>
+            <th>Name</th>
+            <th>Email</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>1</td>
+            <td>Ronaldo</td>
+            <td>ronaldo@gmail.com</td>
+          </tr>
+          <tr>
+            <td>2</td>
+            <td>Messi</td>
+            <td>messi@gmail.com</td>
+          </tr>
+          <tr>
+            <td>3</td>
+            <td>Mbappe</td>
+            <td>mbappe@gmail.com</td>
+          </tr>
+          <tr>
+            <td>4</td>
+            <td>Bale</td>
+            <td>bale@gmail.com</td>
+          </tr>
+          <tr>
+            <td>5</td>
+            <td>Neymar</td>
+            <td>neymar@gmail.com</td>
+          </tr>
+        </tbody>
+    </table>
+</div>
+</body>
+</html>
